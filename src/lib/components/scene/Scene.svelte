f68cbc3a110f34bd01bd31088a90d8227c2a7c2d
LukoOG <emmanueladesipe01@gmail.com>
2026-04-21 20:44:25 +0100

    feat: created scene

---
+++ src/lib/components/scene/Scene.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Canvas, T } from '@threlte/core';
+	import Cube from '../cube/CubeMesh.svelte';
+</script>
+
+<div class="scene-root">
+	<Canvas>
+		<T.PerspectiveCamera makeDefault position={[0, 0, 5]} fov={5} />
+		
+        <T.AmbientLight intensity={0.3} />
+		<T.PointLight position={[3, 5, 3]} intensity={2} color="#ffffff" />
+		<T.PointLight position={[-3, -2, -3]} intensity={0.8} color="#4040ff" />
+
+		<Cube />
+	</Canvas>
+</div>
+
+<style>
+	.scene-root {
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		inset: 0;
+		z-index: 0;
+	}
+</style>
