3815874f4373009d74579fff78e9cedf79a2203f
LukoOG <emmanueladesipe01@gmail.com>
2026-05-18 21:45:14 +0100

    feat: created rust project for wasm
    :

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -28,7 +28,7 @@
 	const handleKeyDown = (e: KeyboardEvent) => {
 		if (e.target instanceof HTMLInputElement) return;
 		if (e.key === 'ArrowLeft') navigateTo('contact', '/contact', true);
-		else if (e.key === 'ArrowRight') navigateTo('hero', '/contact', true);
+		else if (e.key === 'ArrowRight') navigateTo('hero', '/', true);
 	};
 </script>
 
